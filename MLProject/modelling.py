3cd378ca583d1949ea16da0e92f07ddc70a9628a
Rizki Yanuar <ryanuar83@gmail.com>
2026-05-19 09:07:24 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,6 +8,7 @@ from sklearn.ensemble  import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
 
+# Load env key
 load_dotenv()
 
 # Load dan splitting data
